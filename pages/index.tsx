89bd83c40e731f20983d42b2d4c50ab1435dae2a
MariaTeo <marya.teodoraa@gmail.com>
2022-10-25 23:10:25 +0100

    feat: clean up, change favicon with my logo fix #3

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
-  return <h1>Tattoo Parlour</h1>;
-}
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <img
+        src='/asset/image/logo.png'
+        alt='logo'
+        width='50'
+        height='50'
+      />
+      <h1>Tattoo Parlour</h1>;
+    </div>
+  );
+};
+
+export default Home;
