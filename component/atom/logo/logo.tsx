6fb5e280b47c0e424095c3335ca028a52e70bac1
MariaTeo <marya.teodoraa@gmail.com>
2022-11-02 10:24:25 +0000

    feat: swap with next image

--- component/atom/logo/logo.tsx
+++ component/atom/logo/logo.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import Image from 'next/image';
 import { Anchor } from '../anchor';
 import * as S from './logo.style';
 import type * as T from './logo.type';
@@ -7,7 +8,12 @@ import type * as T from './logo.type';
 const Logo = ({ altLogo, logo }: T.Logo) => {
   return (
     <Anchor title='Go to the home page' href='/'>
-      <S.Logo aria-label={altLogo} logo={logo} />
+      <Image
+        alt={altLogo || 'The logo of the page'}
+        src='/asset/image/logo.png'
+        width='24'
+        height='24'
+      />
     </Anchor>
   );
 };
