abc9aff8116fe3f2ec3718e5ca4f45ea681a9284
MariaTeo <marya.teodoraa@gmail.com>
2022-11-03 23:52:38 +0000

    feat: nice

--- component/atom/hero-image-list/hero-image-list.tsx
+++ component/atom/hero-image-list/hero-image-list.tsx
@@ -6,7 +6,7 @@ import type * as T from './hero-image-list.type';
 const HeroImageList = ({ imgUrl, alt, description }: T.HeroImageList) => {
   return (
     <S.HeroList
-      aria-label={alt}
+      aria-label='Just some pictures with the technique of tebori'
       imgUrl={imgUrl}
       data-content={description}
     ></S.HeroList>
